8877e4736fc8afb3c4005dab10bca5a4c1dbbc43
Pavel Artemev <damention@MacBook-Pro.local>
2025-02-12 21:13:47 +0100

    second step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,25 +1,58 @@
 package hexlet.code;
 
+import hexlet.code.repository.BaseRepository;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import io.javalin.Javalin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
 
 public class App {
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "5432");
         return Integer.valueOf(port);
     }
+    public static String getDatabaseUrl() {
+        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+    }
 
-    public static Javalin getApp() throws IOException {
+    private static String readResourceFile(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+
+    public static Javalin getApp() throws IOException, SQLException {
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(getDatabaseUrl());
+
+        var dataSource = new HikariDataSource(hikariConfig);
+
+        var sql = readResourceFile("schema.sql");
+
         Logger logger = LoggerFactory.getLogger(App.class);
-        logger.info("Hello World");
+        logger.info(sql);
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+        BaseRepository.dataSource = dataSource;
+
         return Javalin.create().get("/", ctx -> ctx
                 .result("Hello World"));
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
         Javalin app = getApp();
         app.start(getPort());
     }
---
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,20 @@
+package hexlet.code.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+public class Url {
+    private long id;
+    private String name;
+    private LocalDateTime createdAt;
+
+    public Url(String name) {
+        this.name = name;
+        this.createdAt = LocalDateTime.now();
+    }
+}
