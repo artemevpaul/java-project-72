0e36ae75ac1c1a47ca8562f67fb5aa52af130fed
Pavel Artemev <damention@MacBook-Pro.local>
2025-02-19 21:32:34 +0100

    added tests

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -4,7 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 
+import hexlet.code.model.Url;
 import hexlet.code.repository.BaseRepository;
 import hexlet.code.repository.UrlRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -33,6 +35,27 @@ public class AppTest {
         JavalinTest.test(app, (server, client) -> {
             var response = client.get("/urls");
             assertThat(response.code()).isEqualTo(200);
+        });
+    }
+    @Test
+    public void testCreateUrl() {
+        JavalinTest.test(app, (server, client) -> {
+            var requestBody = "url=https://www.youtube.com";
+            var response = client.post("/urls", requestBody);
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string()).contains("https://www.youtube.com");
+        });
+    }
+
+    @Test
+    public void testUrlPage() throws SQLException {
+        var url = new Url("https://www.youtube.com");
+        url.setCreatedAt(LocalDateTime.now());
+        UrlRepository.save(url);
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/urls/" + url.getId());
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string()).contains("https://www.youtube.com");
         });
     }
 }
